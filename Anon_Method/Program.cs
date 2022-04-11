de70988b642508fd2f435208d89756fbaf460419
Зорин <@K218PC1>
2022-04-11 10:19:39 +0300

    Добавьте файлы проекта.

---
+++ Anon_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anon_Method
+{
+    public delegate void MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate mD = new MyDelegate(delegate { Console.WriteLine("Hello World!"); });
+            mD();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Anon_Method1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anon_Method1
+{
+    public delegate int AddDelegate(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddDelegate aD = new AddDelegate(delegate (int num1, int num2)
+            {
+                int res = num1 + num2;
+                return res;
+            });
+            int sum = aD(5, 4);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
